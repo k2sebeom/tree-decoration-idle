a07e9df4fe62bf087d90a3cf553c6c5c0c93d73b
k2sebeom <moses97@gmail.com>
2023-12-21 12:01:23 +0900

    feat: Add more props

--- src/utils/ornaments.ts
+++ src/utils/ornaments.ts
@@ -10,6 +10,21 @@ import bell6Img from '../assets/props/bell6.png'
 import bell7Img from '../assets/props/bell7.png'
 import bell8Img from '../assets/props/bell8.png'
 import bell9Img from '../assets/props/bell9.png'
+import bell10Img from '../assets/props/bell10.png'
+import bell11Img from '../assets/props/bell11.png'
+import bell12Img from '../assets/props/bell12.png'
+import bell14Img from '../assets/props/bell14.png'
+import bell15Img from '../assets/props/bell15.png'
+import bell16Img from '../assets/props/bell16.png'
+import bell17Img from '../assets/props/bell17.png'
+import bell18Img from '../assets/props/bell18.png'
+import bell19Img from '../assets/props/bell19.png'
+import bell20Img from '../assets/props/bell20.png'
+import bell21Img from '../assets/props/bell21.png'
+import bell22Img from '../assets/props/bell22.png'
+import bell23Img from '../assets/props/bell23.png'
+import bell24Img from '../assets/props/bell24.png'
+import bell25Img from '../assets/props/bell25.png'
 
 import snowman1 from '../assets/props/snowman1.png'
 import snowman2 from '../assets/props/snowman2.png'
@@ -17,11 +32,28 @@ import snowman3 from '../assets/props/snowman3.png'
 import snowman4 from '../assets/props/snowman4.png'
 import snowman5 from '../assets/props/snowman5.png'
 
+import santa1 from '../assets/props/santa1.png'
+
+import deer1 from '../assets/props/deer1.png'
+
+import duck1 from '../assets/props/duck1.png'
+
+import gift1 from '../assets/props/gift1.png'
+import gift2 from '../assets/props/gift2.png'
+import gift3 from '../assets/props/gift3.png'
+import gift4 from '../assets/props/gift4.png'
+import gift5 from '../assets/props/gift5.png'
+
+import plant1 from '../assets/props/plant1.png'
+
 import candle1 from '../assets/props/candle1.png'
 
 import star1 from '../assets/props/star1.png';
 import star2 from '../assets/props/star2.png'
+import star3 from '../assets/props/star3.png'
 
+import tree1 from '../assets/props/tree1.png';
+import tree2 from '../assets/props/tree2.png'
 
 interface OrnamentType {
     source: string;
@@ -75,6 +107,71 @@ export const NAME_MAPPING: Record<string, OrnamentType> = {
       source: bell9Img,
       width: 30,
     },
+    'bell10': {
+      source: bell10Img,
+      width: 35,
+    },
+    'bell11': {
+      source: bell11Img,
+      width: 40,
+    },
+    'bell12': {
+      source: bell12Img,
+      width: 35,
+    },
+    'bell14': {
+      source: bell14Img,
+      width: 35,
+    },
+    'bell15': {
+      source: bell15Img,
+      width: 35,
+    },
+    'bell16': {
+      source: bell16Img,
+      width: 35,
+    },
+    'bell17': {
+      source: bell17Img,
+      width: 35,
+    },
+    'bell18': {
+      source: bell18Img,
+      width: 35,
+    },
+    'bell19': {
+      source: bell19Img,
+      width: 30,
+    },
+    'bell20': {
+      source: bell20Img,
+      width: 30,
+    },
+    'bell21': {
+      source: bell21Img,
+      width: 30,
+    },
+    'bell22': {
+      source: bell22Img,
+      width: 30,
+    },
+    'bell23': {
+      source: bell23Img,
+      width: 30,
+    },
+    'bell24': {
+      source: bell24Img,
+      width: 30,
+    },
+    'bell25': {
+      source: bell25Img,
+      width: 30,
+    },
+
+    'plant1': {
+      source: plant1,
+      width: 30,
+    },
 
     'snowman1': {
       source: snowman1,
@@ -97,6 +194,51 @@ export const NAME_MAPPING: Record<string, OrnamentType> = {
       width: 30,
     },
 
+    'santa1': {
+      source: santa1,
+      width: 30,
+    },
+
+    'tree1': {
+      source: tree1,
+      width: 30,
+    },
+    'tree2': {
+      source: tree2,
+      width: 30,
+    },
+
+    'deer1': {
+      source: deer1,
+      width: 35,
+    },
+
+    'duck1': {
+      source: duck1,
+      width: 35,
+    },
+
+    'gift1': {
+      source: gift1,
+      width: 35,
+    },
+    'gift2': {
+      source: gift2,
+      width: 35,
+    },
+    'gift3': {
+      source: gift3,
+      width: 35,
+    },
+    'gift4': {
+      source: gift4,
+      width: 35,
+    },
+    'gift5': {
+      source: gift5,
+      width: 35,
+    },
+
     'star1': {
       source: star1,
       width: 30,
@@ -104,5 +246,9 @@ export const NAME_MAPPING: Record<string, OrnamentType> = {
     'star2': {
       source: star2,
       width: 30,
+    },
+    'star3': {
+      source: star3,
+      width: 35,
     },
 }
